42867181ab739971d6ff985834c9c99186d40e89
Formasitchijoh <formasitf@gmail.com>
2023-11-09 07:32:07 +0100

    first commit

---
+++ src/components/formasit/inputField.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomInput = () => {
+  return (
+    <div className="container max-w-sm">
+        <input type="text"  className='border border-slate-300 rounded-md py-4 w-full '  />
+    </div>
+  )
+}
+
+export default CustomInput
